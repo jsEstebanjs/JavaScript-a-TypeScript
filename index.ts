dfcbf4b03e89a09ffd8545d1ac9804b9956139c3
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-10-14 10:54:54 -0500

    end

---
+++ index.ts
@@ -0,0 +1,35 @@
+type wallet = {
+    number:string,
+    balance:number
+}
+const accounts:wallet[] = [];
+
+function open(number:string, balance:number):void {
+    accounts.push({ number , balance })
+}
+function getBalance(number:string): number {
+    const account: wallet | undefined = accounts.find(account => account.number === number)
+    if (!account) throw new Error("La cuenta no se encontró")
+    return account.balance
+}
+function deposit(number:string, amount:number):void {
+    const account : wallet | undefined = accounts.find(account => account.number )
+    if (!account) throw new Error("La cuenta no se encontró")
+    account.balance += amount
+}
+
+function withdraw(number:string, amount:number):void {
+    const account : wallet | undefined = accounts.find(account => account.number )
+    if (!account) throw new Error("La cuenta no se encontró")
+    if (account.balance - amount < 0) throw new Error("Fondos insuficientes")
+    account.balance -= amount
+}
+
+
+open("111", 0)
+
+deposit("111", 1000)
+console.log(getBalance("111"))
+
+withdraw("111", 500)
+console.log(getBalance("111"))
